bb887afb17891da80a71469b12fe6dca4ddd4fc2
d4tk1dmVn <datkidmun@gmail.com>
2025-03-17 13:11:25 +0100

    Scrap shunting yard, add initial logic for simpler calculator

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 const display = document.querySelector(".display");
 display.textContent = "this WAS your calculator";
+let exec_stack = [undefined, undefined, undefined];
 
 const checkInput = (input) => {
   return typeof input === "number";
@@ -9,67 +10,64 @@ const add = (a, b) => {
   if (checkInput(a) && checkInput(b)) {
     return a + b;
   }
-  return Nan;
+  return NaN;
 };
 
 const subtract = (a, b) => {
   if (checkInput(a) && checkInput(b)) {
     return a - b;
   }
-  return Nan;
+  return NaN;
 };
 
 const multiply = (a, b) => {
   if (checkInput(a) && checkInput(b)) {
     return a * b;
   }
-  return Nan;
-}
+  return NaN;
+};
 
 const divide = (a, b) => {
   if (checkInput(a) && checkInput(b)) {
     return a / b;
   }
-  return Nan;
+  return NaN;
 };
 
-const shuntingYard = (array) => {
-  // let stack = [];
-  // let result = [];
-  // for (let i = 0; i < array.length ; i++){
-  //   const entry = array[i];
-  //   if (typeof entry === "number"){
-  //     result.push(entry)
-  //   } else if ("+/*-".includes)
-  // }
-  // return result
-  return array
+const validOperation = (exec_stack) => {
+  return (
+    exec_stack.length === 3 &&
+    typeof exec_stack[0] === "number" &&
+    exec_stack[1] === "number" &&
+    "*+/-".includes(exec_stack[2])
+  );
 };
 
-const toArray = (string) => {
-  let result = [];
-  let number = "";
-  for(let i = 0; i < string.length; i++){
-    if("+/*-".includes(string.charAt(i))){
-      result.push(Number(number));
-      result.push(string.charAt(i));
-      number = "";
-    } else {
-      number += string.charAt(i);
-    }
+const operate = (exec_stack) => {
+  if (!validOperation(exec_stack)) {
+    return "SYNTAX ERROR";
   }
-  result.push(number);
-  return result;
-};
-
-const solve = (polish_notation) => {
-  return polish_notation;
-};
-
-const evaluate = (input) => {
-  const array = toArray(input);
-  const polish_notation = shuntingYard(array);
-  return solve(polish_notation).toString();
+  const a = exec_stack[0];
+  const b = exec_stack[1];
+  const operand = exec_stack[2];
+  switch (operand) {
+    case "*":
+      exec_stack[0] = multiply(a, b);
+      break;
+    case "+":
+      exec_stack[0] = add(a, b);
+      break;
+    case "-":
+      exec_stack[0] = subtract(a, b);
+      break;
+    case "/":
+      exec_stack[0] = divide(a, b);
+      break;
+    default:
+      exec_stack[0] = NaN;
+      break;
+  }
+  return exec_stack[0];
 };
 
 const print = (evt) => {
@@ -79,12 +77,22 @@ const print = (evt) => {
       break;
     case "Clear":
       display.textContent = "";
+      exec_stack = [undefined, undefined, undefined];
       break;
     case "=":
-      display.textContent = evaluate(display.textContent);
+      exec_stack[1] = Number(display.textContent);
+      display.textContent = operate(exec_stack).toString();
       break;
     default:
-      display.textContent += evt.currentTarget.textContent;
+      if ("+*/-".includes(evt.currentTarget.textContent)) {
+        // WHAT HAPPENS IF WE ARE SKIPPING THE USE OF =?
+        exec_stack[0] = Number(display.textContent);
+        exec_stack[2] = evt.currentTarget.textContent;
+      } else if (exec_stack[2] !== undefined) {
+        display.textContent = "" + evt.currentTarget.textContent;
+      } else {
+        display.textContent += evt.currentTarget.textContent;
+      }
       break;
   }
 };
